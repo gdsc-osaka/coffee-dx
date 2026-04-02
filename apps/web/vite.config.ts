e517c68941053983c1f1d3f85a69328b2594eaac
Sakura584 <sakura412629@gmail.com>
2026-04-02 20:55:16 +0900

    fix: react-router build で先にビルド,workers/appをmainに指定

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,11 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
+    mode === "development" && cloudflare({ viteEnvironment: { name: "ssr" } }),
     tailwindcss(),
     reactRouter(),
     tsconfigPaths(),
   ],
-});
+}));
