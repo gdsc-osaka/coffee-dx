29a238a2583f26d20f32589ee8d3a8f6eb1db341
Sakura584 <sakura412629@gmail.com>
2026-04-03 00:23:55 +0900

    fix: /web/vite.config.tsの拡張子をmtsに変更

--- apps/web/app/_cashier.tsx
+++ apps/web/app/_cashier.tsx
@@ -1,7 +1,7 @@
-import { redirect, Outlet } from "react-router";
+import { Outlet } from "react-router";
 import type { Route } from "./+types/_cashier";
 
-export async function loader({ request }: Route.LoaderArgs) {
+export async function loader({ request: _request }: Route.LoaderArgs) {
   // TODO: スタッフ認証を実装する
   // const isStaff = await verifyStaffSession(request);
   // if (!isStaff) throw redirect("/");
